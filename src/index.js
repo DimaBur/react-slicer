7498fad9baf1b31e2f27429d1e31c2676fd0e063
DimaBur <buravtsov.dm@yandex.ru>
2018-05-24 18:15:19 +0300

    wip render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 class Slicer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: ['a','b','c','d','e','f','g','h','i','j','k'],
+      currentPage: 1,
+      itemsPerPage: 3
+    }
+  }
+
   render() {
+    const {items, currentPage, itemsPerPage} = this.state;
+
+    const lastItemIndex = currentPage * itemsPerPage;
+    const firstItemIndex = lastItemIndex - itemsPerPage;
+    const currentItems = items.slice(firstItemIndex, lastItemIndex);
+
+    console.log(currentItems)
+
+    const renderItems = currentItems.map((item, index) => <div key={index}>{item}</div>);
+
     return (
-        <div>Slicer component</div>
+        <div>{renderItems}</div>
     );
   }
 }
+
 export default Slicer;
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        //include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
