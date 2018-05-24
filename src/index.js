fa83c29309c83916ccc4e95e6ae0270fd57584ff
DimaBur <buravtsov.dm@yandex.ru>
2018-05-24 19:06:17 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -10,19 +10,45 @@ class Slicer extends React.Component {
     }
   }
 
+  handlePageNumberClick(e) {
+    this.setState({
+      currentPage: Number(e.target.id)
+    })
+  }
+
+  renderItems(items, currentPage, itemsPerPage) {
+    const lastItemIndex = currentPage * itemsPerPage,
+        firstItemIndex = lastItemIndex - itemsPerPage,
+        currentItems = items.slice(firstItemIndex, lastItemIndex);
+
+    return currentItems.map((item, index) => <div key={index}>{item}</div>);
+  }
+
+  renderPagination(items, itemsPerPage) {
+    let pagination = [];
+    for (let i = 1; i <= Math.ceil(items.length / itemsPerPage); i++) {
+      pagination.push(i);
+    }
+    return pagination.map(number =>
+          <li
+              key={number}
+              id={number}
+              onClick={(e) => this.handlePageNumberClick(e)}
+          >
+            {number}
+          </li>
+      );
+  }
+
   render() {
     const {items, currentPage, itemsPerPage} = this.state;
 
-    const lastItemIndex = currentPage * itemsPerPage;
-    const firstItemIndex = lastItemIndex - itemsPerPage;
-    const currentItems = items.slice(firstItemIndex, lastItemIndex);
-
-    console.log(currentItems)
-
-    const renderItems = currentItems.map((item, index) => <div key={index}>{item}</div>);
-
     return (
-        <div>{renderItems}</div>
+        <div className={'react-slicer'}>
+
+        <div className={'react-slicer__wrapper'}>{this.renderItems(items, currentPage, itemsPerPage)}</div>
+          <ul>{this.renderPagination(items, itemsPerPage)}</ul>
+        </div>
     );
   }
 }
